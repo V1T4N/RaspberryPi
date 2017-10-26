b74eebec01309e5c83e76047aa2c4f059fe6e277
VITAN <admin@MacBook-Pro.local>
2017-10-26 17:07:11 +0900

    test2

---
+++ UDP.py
@@ -0,0 +1,16 @@
+import socket
+from getch import getch
+import time
+
+UDP_IP = "192.168.0.12"
+UDP_PORT = 5005
+#MESSAGE = "Hello, World!"
+
+
+while True:
+
+ MESSAGE = getch()
+ 
+ sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+ sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
